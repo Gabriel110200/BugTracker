43316c77a992c00bbac45c2bf0b423b87c0d969a
Gabriel <gb_souza@id.uff.br>
2022-10-26 20:12:18 -0300

    add Team Model

---
+++ ProjectManagement/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectManagement.Models
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+
+    }
+}
---
+++ ProjectManagement/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectManagement.Models
+{
+    public class Team
+    {
+        public string Name { get; set; }
+
+        public Enum Rating { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public List<IdentityUser> Users { get; set; }
+
+      
+
+    }
+}
