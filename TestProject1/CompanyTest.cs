212b27c91c798a51ecbfa0ccbfe3fc98027ceda9
Gabriel <gb_souza@id.uff.br>
2023-01-01 10:22:00 -0300

    add company already exists test

--- TestProject1/CompanyTest.cs
+++ TestProject1/CompanyTest.cs
@@ -21,6 +21,16 @@ namespace TestProject1
             Connect connect = new Connect();
             this.context = connect.CriarContextInMemory();
             this.context.Database.EnsureDeleted();
+            PrepareDatabase();
+        }
+
+     
+
+        [TestMethod]
+        public async Task CreateCompanyIsValid()
+        {
+
+
 
         }
 
@@ -54,6 +64,53 @@ namespace TestProject1
         }
 
 
+
+        [TestMethod]
+
+        public async Task CreateCompanyAlreadyExists()
+        {
+            PrepareDatabase();
+
+            try
+            {
+                var service =new CompanyService(this.context);
+
+                var company = new Company()
+                {
+                    UserId = "59cc8c06-319a-424f-843d-aa66deed3c00",
+                    Name = "Empresa Tester",
+                    CNPJ = "111111111",
+
+                };
+
+                await service.Create(company);
+
+                Assert.Fail();
+
+            }catch(Exception ex)
+            {
+
+                Assert.AreEqual("Company already Registered", ex.Message);
+
+            }
+
+
+        }
+
+        private  void PrepareDatabase()
+        {
+
+            var company = new Company()
+            {
+                UserId = "59cc8c06-319a-424f-843d-aa66deed3c00",
+                Name = "Empresa Test",
+                CNPJ = "111111111",
+
+            };
+
+
+             this.context.Companies.Add(company);
+        }
 
     }
 }
